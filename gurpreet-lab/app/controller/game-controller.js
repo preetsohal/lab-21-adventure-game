5c2c0b20ba706d4e1c325bd72cae38b16bfa7cc2
preetsohal <preetsohal@live.com>
2016-08-21 23:28:09 -0700

    tried to add some features

--- gurpreet-lab/app/controller/game-controller.js
+++ gurpreet-lab/app/controller/game-controller.js
@@ -5,10 +5,14 @@ const demoApp = angular.module('demoApp');
 
 demoApp.controller('GameController', ['$log', GameController]);
 
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
 function GameController($log){
   this.history =[{ id: 0, text: 'you are on a gooy adventure'}];
   this.directions = ['north', 'south', 'east', 'west'];
-
+  this.prize = ['Bronze', 'Ruby', 'gold', 'Diamond'];
   this.player = {
     name: 'percilla',
     hp: 100,
@@ -16,6 +20,15 @@ function GameController($log){
   };
 
   this.map = require('../lib/map.js');
+
+  this.addItemToBackpack = function(number){
+    if (number < 6){
+      let newPrize = this.prize.pop();
+      this.player.backpack.push(newPrize);
+      return 'Prize? Yes! You got a ' + newPrize;
+    }
+    else return 'Prize? No! No candy for you!';
+  };
 
   this.moveDirection = function(direction){
     $log.debug('gameCtrl.moveDirection');
@@ -27,7 +40,7 @@ function GameController($log){
       $log.log('nextRoom', nextRoom);
       if( nextRoom !== 'wall') {
         this.player.location = nextRoom;
-        this.logHistory('you have entered' + this.player.location);
+        this.logHistory('you have entered' + this.player.location + '. ' + this.addItemToBackpack(getRandomNumber(1, 10)));
         return;
       }
       this.logHistory('you hit the wall');
--- gurpreet-lab/test/game-controller-test.js
+++ gurpreet-lab/test/game-controller-test.js
@@ -13,14 +13,25 @@ describe('testing GameController', function(){
     this.gameCtrl.history = [];
     this.gameCtrl.player = {
       name: 'slug',
-      location: 'roomD'
+      location: 'roomB'
+      backPack: ['Diamond']
     };
   });
 
   it('the player should change locations', () => {
     this.gameCtrl.moveDirection('east');
-    expect(this.gameCtrl.player.location).toBe('roomD');
+    expect(this.gameCtrl.player.location).toBe('roomB');
+    expect(this.gameCtrl.history[0].text).toContain('you have entered the roomB');
+    expect(this.ganeCtrl.history[0].text).toContain('prize?');
   });
+
+  it('the palyer should hit wall', () => {
+    this.gameCtrl.moveDirection('north');
+    // expect(this.gameCtrl.player.location).toBe('roomD');
+    expect(this.gameCtrl.history[0].text).toBe('you hit the wall');
+    expect(this.ganeCtrl.history[0].text).not.toContain('prize?');
+  });
+});
 
 
 });
